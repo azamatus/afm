44d3918c8b1d69a4e1d26858e75c8efc0a88744f
Aza <azamatkurmanbekov@gmail.com>
2012-12-15 16:49:08 +0600

    header

---
+++ src/Nurix/NurixBundle/Resources/public/js/header.js
@@ -0,0 +1,16 @@
+function del()
+{
+
+    if(document.forma.poisk.value=='Поиск...')
+    {
+        document.forma.poisk.value='';
+    }
+}
+function dob()
+{
+    if(document.forma.poisk.value=='')
+    {
+        document.forma.poisk.value='Поиск...';
+    }
+
+}
