c9368d8cd99bb62d54be20561249413e6a9610da
Nurlanbek Moldomurov <bupychuk1989@gmail.com>
2012-12-22 17:48:27 +0600

    menu

--- src/Nurix/NurixBundle/Menu/MenuBuilder.php
+++ src/Nurix/NurixBundle/Menu/MenuBuilder.php
@@ -21,8 +21,8 @@ class MenuBuilder
         $menu = $this->factory->createItem('main_menu');
 
         $menu->addChild('home', array('route' => 'nurix_homepage','label'=>'Главная'));
-        $menu->addChild('contacts', array('route' => 'nurix_default','label'=>'Контакты'));
-        $menu->addChild('sitemap', array('route' => 'nurix_default','label'=>'Карта сайта'));
+        $menu->addChild('contacts', array('route' => 'nurix_default', 'routeParameters' => array('name' => 'Nurlan'),'label'=>'Контакты'));
+        $menu->addChild('sitemap', array('route' => 'nurix_default', 'routeParameters' => array('name' => 'Nurlan'),'label'=>'Карта сайта'));
         $menu->setChildrenAttribute('class','main_menu');
         return $menu;
     }
@@ -31,14 +31,14 @@ class MenuBuilder
     {
         $menu = $this->factory->createItem('sidebar');
         $menu->setChildrenAttribute('class','side_menu');
-        $menu->addChild('Catalog', array('route' => 'nurix_default','label'=>'Каталог'))
+        $menu->addChild('Catalog', array('route' => 'nurix_default', 'routeParameters' => array('name' => 'Nurlan'),'label'=>'Каталог'))
             ->setAttribute('class','catalog_menu')
             ->setLinkAttribute('class','f20');
-        $menu->addChild('audio', array('route' => 'nurix_default','label'=>'Аудиотехника'));
+        $menu->addChild('audio', array('route' => 'nurix_default', 'routeParameters' => array('name' => 'Nurlan'),'label'=>'Аудиотехника'));
         $menu->addChild('video', array('route' => 'nurix_homepage','label'=>'Видеотехника'));
-        $menu->addChild('comp', array('route' => 'nurix_default','label'=>'Компьютеры'))
-            ->addChild('comp_1', array('route' => 'nurix_default','label'=>'Комплектующие'));
-        $menu->addChild('mobile', array('route' => 'nurix_default','label'=>'Сотовые телефоны'));
+        $menu->addChild('comp', array('route' => 'nurix_default', 'routeParameters' => array('name' => 'Nurlan'),'label'=>'Компьютеры'))
+            ->addChild('comp_1', array('route' => 'nurix_default', 'routeParameters' => array('name' => 'Nurlan'),'label'=>'Комплектующие'));
+        $menu->addChild('mobile', array('route' => 'nurix_default', 'routeParameters' => array('name' => 'Nurlan'),'label'=>'Сотовые телефоны'));
 
         return $menu;
     }
