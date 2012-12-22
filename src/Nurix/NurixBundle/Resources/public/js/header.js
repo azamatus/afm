a7cf965e8e2752ec09d0eafdb732b8fc4069e8ee
Meder <juve-meder@mail.ru>
2012-12-22 15:18:43 +0600

    footer gotov

--- src/Nurix/NurixBundle/Resources/public/js/header.js
+++ src/Nurix/NurixBundle/Resources/public/js/header.js
@@ -12,5 +12,19 @@ function dob()
     {
         document.forma.poisk.value='Поиск...';
     }
+}
+function delete_value()
+{
 
+    if(document.forma2.podpiska.value==' Введите ваш электронный адрес')
+    {
+        document.forma2.podpiska.value='';
+    }
+}
+function add_value()
+{
+    if(document.forma2.podpiska.value=='')
+    {
+        document.forma2.podpiska.value=' Введите ваш электронный адрес';
+    }
 }
