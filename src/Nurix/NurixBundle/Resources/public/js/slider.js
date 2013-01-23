e6dea841b8e43a965ec4d1a58dc74b3d77ee62b5
Aza <azamatkurmanbekov@gmail.com>
2013-01-23 13:28:07 +0400

    content_slider

---
+++ src/Nurix/NurixBundle/Resources/public/js/slider.js
@@ -0,0 +1,66 @@
+jQuery(function($)
+{
+    $("#carousel").rcarousel(
+        {
+            visible: 1,
+            step: 1,
+            speed: 700,
+            auto:
+            {
+                enabled: false,
+                interval: 2000
+            },
+            width: 700,
+            height: 345,
+            start: generatePages,
+            pageLoaded: pageLoaded
+
+        }
+    );
+    function generatePages()
+    {
+        var _total, i, _link;
+
+        _total = $( "#carousel" ).rcarousel( "getTotalPages" );
+
+        for ( i = 0; i < _total; i++ )
+        {
+            _link = $( "<a href='#'></a>" );
+
+            $(_link)
+                .bind("click", {page: i},
+                function( event )
+                {
+                    $( "#carousel" ).rcarousel( "goToPage", event.data.page );
+                    event.preventDefault();
+                }
+            )
+                .addClass("bullet off" )
+                .appendTo("#pages" );
+        }
+
+        $( "a:eq(0)", "#pages" )
+            .removeClass("off" )
+            .addClass("on" )
+            .addClass("bullet2");
+            //.css("background-image", "url('page-on.png')" );
+    }
+    function pageLoaded( event, data )
+    {
+        $( "a.on", "#pages" )
+            .removeClass("on" )
+            .removeClass("bullet2" )
+            .addClass("bullet");
+            //.css("background-image", "url('page-off.png')" );
+
+        $( "a", "#pages" )
+            .eq( data.page )
+            .removeClass("bullet" )
+            .addClass( "on" )
+            .addClass("bullet2");
+            //.css("background-image", "url('page-on.png')" );
+    };
+
+
+
+});
