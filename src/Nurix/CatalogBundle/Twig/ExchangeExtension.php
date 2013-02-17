155892a7f5426b876775d55f96624eb37f6be486
Aza <azamatkurmanbekov@gmail.com>
2013-02-17 14:06:51 +0600

    exchange

---
+++ src/Nurix/CatalogBundle/Twig/ExchangeExtension.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: Admin
+ * Date: 17.02.13
+ * Time: 12:50
+ * To change this template use File | Settings | File Templates.
+ */
+namespace Nurix\CatalogBundle\Twig;
+
+class ExchangeExtension extends \Twig_Extension
+{
+    public function getFilters()
+    {
+        return array(
+            'price' => new \Twig_Filter_Method($this, 'priceFilter'),
+        );
+    }
+
+    public function priceFilter($number,$exchange, $decimals = 0, $decPoint = '.')
+    {
+        if($exchange == 'USD')
+        {
+            $price = number_format($number, $decimals, $decPoint,'');
+            $price = '$' . $price;
+        }
+        elseif ($exchange == 'SOM')
+        {
+            // TODO Надо исправить, чтобы брать из базы
+            $price = number_format($number*47.8, $decimals, $decPoint,'');
+            $price = $price.' сом';
+        }
+        else
+        {
+            throw new \Symfony\Component\DependencyInjection\Exception\InvalidArgumentException('Не правильная валюта!'.$exchange);
+
+        }
+        return $price;
+    }
+
+    public function getName()
+    {
+        return 'exchange_extension';
+    }
+}
