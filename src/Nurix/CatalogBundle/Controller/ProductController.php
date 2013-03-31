5170d2158ee212452966a2acdc187b03a8051f39
Aza <azamatkurmanbekov@gmail.com>
2013-03-31 14:38:32 +0600

    verstka harakteristika gotova

--- src/Nurix/CatalogBundle/Controller/ProductController.php
+++ src/Nurix/CatalogBundle/Controller/ProductController.php
@@ -14,12 +14,16 @@ class ProductController extends Controller
             ->getRepository('CatalogBundle:Goods')
             ->find($id);
 
+        $mainchar = $this -> getDoctrine()
+            ->getRepository("CatalogBundle:Characteristic")
+            ->findBy(array("goodId"=>$id), null, 8);
+
         $repository= $this->getDoctrine()
             ->getRepository("CatalogBundle:Characteristic");
 
         $char = $repository->getGoodCharacteristic($id);
 
-        return $this->render('CatalogBundle:Product:product_info.html.twig', array('product' => $entity, 'char'=>$char));
+        return $this->render('CatalogBundle:Product:product_info.html.twig', array('product' => $entity, 'char'=>$char, 'mainchar' => $mainchar));
     }
     public function getSameAction()
     {
