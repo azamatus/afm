fc981c3fc5633a5ce9587e6e99ff55a3c15398b8
Nurlanbek Moldomurov <bupychuk1989@gmail.com>
2013-04-15 03:33:52 +0600

    NUR-23 MediaExtension

---
+++ src/Application/Sonata/MediaBundle/Twig/ThumbnailExtension.php
@@ -0,0 +1,73 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: Admin
+ * Date: 17.02.13
+ * Time: 12:50
+ * To change this template use File | Settings | File Templates.
+ */
+namespace Application\Sonata\MediaBundle\Twig;
+
+use Application\Sonata\MediaBundle\Entity\Gallery;
+use Sonata\MediaBundle\Model\MediaInterface;
+use Sonata\MediaBundle\Twig\Extension\MediaExtension;
+
+class ThumbnailExtension extends MediaExtension
+{
+
+    public function getFilters()
+    {
+        return array(
+            'galleryThumb' => new \Twig_Filter_Method($this, 'galleryThumbnailFilter',array(
+                'is_safe' => array('html')
+            )),
+            'thumb' => new \Twig_Filter_Method($this, 'thumbnailFilter',array(
+                'is_safe' => array('html')
+            )),
+        );
+    }
+
+
+    public function galleryThumbnailFilter($gallery,$noImagePath,$format='big',$options = array())
+    {
+        if (!$gallery||!$gallery->getGalleryHasMedias())
+        {
+            $provider = $this->getMediaService()
+                ->getProvider('sonata.media.provider.image');
+            $options = array_merge($options,array('src'=>$noImagePath));
+            return $this->render($provider->getTemplate('helper_thumbnail'), array(
+                'options'  => $options,
+            ));
+        }
+        else
+        {
+            $medias = $gallery->getGalleryHasMedias();
+
+            return $this->thumbnail($medias[0],$format,$options);
+        }
+    }
+
+    public function thumbnailFilter( $media,$noImagePath,$format='big',$options = array())
+    {
+        $result = $this->path($media,$format,$options);
+        if (!$result)
+        {
+            $result=$noImagePath;
+
+            $provider = $this->getMediaService()
+                ->getProvider('sonata.media.provider.image');
+            $options = array_merge($options,array('src'=>$result));
+            return $this->render($provider->getTemplate('helper_thumbnail'), array(
+                'options'  => $options,
+            ));
+        }
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getName()
+    {
+        return 'thumb_extension';
+    }
+}
