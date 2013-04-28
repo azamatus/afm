e0e43a08af8e3e81b6323a8cd93cc96e6abc80a3
Turdaliev Nursultan <nursultan2010@gmail.com>
2013-04-29 01:53:28 +0600

    NUR-29 Registered parser as a service.

--- src/Nurix/CatalogBundle/Controller/AdminController.php
+++ src/Nurix/CatalogBundle/Controller/AdminController.php
@@ -8,17 +8,11 @@
  */
 namespace Nurix\CatalogBundle\Controller;
 
-use DateTime;
-use Nurix\CatalogBundle\Entity\Goods;
 use Nurix\CatalogBundle\Form\Type\ExcelType;
 use Sonata\AdminBundle\Controller\CoreController;
 use Symfony\Component\HttpFoundation\File\File;
 use Symfony\Component\HttpFoundation\Request;
-use Doctrine\ORM\EntityNotFoundException;
-use Nurix\CatalogBundle\Entity\Characteristic;
-use Nurix\CatalogBundle\Entity\CharacteristicSection;
-use Nurix\CatalogBundle\Entity\CharacteristicType;
-use Sunra\PhpSimple\HtmlDomParser;
+
 use Symfony\Component\HttpFoundation\Response;
 
 class AdminController extends CoreController
@@ -45,11 +39,11 @@ class AdminController extends CoreController
                     $file->move($dir, $filename);
                     $excel_file = new File($dir . '/' . $filename);
 
-                    $info = $this->parseExcel($excel_file);
-                    $added_message = 'Добавлено: ' .$info['added'];
-                    $updated_message = 'Обновлено: ' .$info['updated'];
-                    $this->get('session')->getFlashBag()->add('notice',$added_message );
-                    $this->get('session')->getFlashBag()->add('notice',$updated_message );
+                    $info = $this->get('catalog.product.parser')->parseExcel($excel_file);
+                    $added_message = 'Добавлено: ' . $info['added'];
+                    $updated_message = 'Обновлено: ' . $info['updated'];
+                    $this->get('session')->getFlashBag()->add('notice', $added_message);
+                    $this->get('session')->getFlashBag()->add('notice', $updated_message);
                 } else {
                     $this->get('session')->getFlashBag()->add('notice', 'Неверный формат');
                 }
@@ -65,147 +59,5 @@ class AdminController extends CoreController
             ));
     }
 
-    public function parseExcel(File $file) //TODO: вынести в отдельный слой
-    {
 
-        $exelObj = $this->get('xls.load_xls2007')->load($file);
-        $sheetData = $exelObj->getActiveSheet()->toArray(null, true, true, true);
-        $index = 0;
-
-        $entityManager = $this->getDoctrine()->getManager();
-
-        $catalogs = $entityManager->getRepository('CatalogBundle:Catalog')->findAll();
-        $goods_alias = array();
-
-        $added_goods = 0;
-        $updated_goods = 0;
-
-        foreach ($catalogs as $catalog) {
-            $goods_alias[$catalog->getId()] = explode(',', $catalog->getGoodsAlias());
-        }
-
-        foreach ($sheetData as $sheetRow) {
-
-            $index++;
-            if ($index == 1) continue;
-            $article = $sheetRow['A'] ? $sheetRow['A'] : null;
-            $name = $sheetRow['B'] ? $sheetRow['B'] : null;
-            $last_update = new DateTime(date('Y-m-d', strtotime($sheetRow['I'])));
-            $price = (float)$sheetRow['L'];
-            $urlYandex = $sheetRow['Q'];
-            $subcatalog = null;
-            if (!empty($article)) {
-
-                foreach ($goods_alias as $catalog_id => $aliases) {
-                    foreach ($aliases as $alias) {
-                        if (!empty($alias))
-                            if (strpos($name, $alias) !== false) {
-                                $subcatalog = $entityManager->getRepository('CatalogBundle:Catalog')->find($catalog_id);
-                                break;
-                            }
-                    }
-                }
-
-                $good = $entityManager->getRepository('CatalogBundle:Goods')->findOneByArticle($article);
-                if ($good) {
-                    $updated_goods++;
-                    $good->setName($name);
-                    $good->setLastUpdate($last_update);
-                    $good->setPrice($price);
-                    $good->setCatalog($subcatalog);
-                    $good->setActive(true);
-                    $entityManager->flush();
-
-
-                } else {
-                    $added_goods++;
-                    $good = new Goods();
-                    $good->setArticle($article);
-                    $good->setName($name);
-                    $good->setActive(true);
-                    $good->setLastUpdate($last_update);
-                    $good->setPrice($price);
-                    $good->setCatalog($subcatalog);
-
-                    $entityManager->persist($good);
-                    $entityManager->flush();
-
-
-                }
-
-                if ($urlYandex){
-                    $characters =$entityManager->getRepository('CatalogBundle:Characteristic')->findByGoodId($good->getId());
-                    if(!$characters){
-                        $this->parseYandex($urlYandex, $good->getId());
-                    }
-                }
-            }
-        }
-        return array('added'=>$added_goods,'updated'=>$updated_goods);
-    }
-
-    public function parseYandex($url, $goodid) //TODO: вынести в отдельный слой
-    {
-        $good = $this->getDoctrine()->getRepository('CatalogBundle:Goods')->find($goodid);
-        if (!$good) {
-            Throw new EntityNotFoundException("good not found");
-        }
-        $html = HtmlDomParser::file_get_html($url);
-        foreach ($html->find('table.l-page_layout_72-20 .b-properties') as $table) {
-            $sid = 0;
-            foreach ($table->find('tr') as $tr) {
-                $section_type = null;
-                $html_span = HtmlDomParser::str_get_html($tr);
-                $section_type = $html_span->find('.b-properties__title', -1);
-
-                if ($section_type != null) {
-                    $section_type = $section_type->plaintext;
-                    $repository = $this->getDoctrine()->getRepository('CatalogBundle:CharacteristicSection');
-                    $characteristic_section = $repository->findOneBySectionvalue($section_type);
-                    if ($characteristic_section) {
-                        $sid = $characteristic_section;
-                    } else {
-                        $section = new CharacteristicSection();
-                        $section->setSectionvalue($section_type);
-
-                        $em = $this->getDoctrine()->getManager();
-                        $em->persist($section);
-                        $em->flush();
-                        $sid = $section;
-                    }
-                } else {
-                    $char_type = $html_span->find('.b-properties__label-title', -1)->plaintext;
-
-                    $char = $html_span->find('.b-properties__value', -1)->plaintext;
-                    $repository = $this->getDoctrine()->getRepository('CatalogBundle:CharacteristicType');
-                    $type = $repository->findOneByName($char_type);
-
-                    if ($type) {
-                        $tid = $type;
-
-                    } else {
-                        $characteristic_type = new CharacteristicType();
-                        $characteristic_type->setName($char_type);
-                        $characteristic_type->setSection($sid);
-
-                        $em = $this->getDoctrine()->getManager();
-                        $em->persist($characteristic_type);
-                        $em->flush();
-                        $tid = $characteristic_type;
-                    }
-                    $characteristic = new Characteristic();
-                    $characteristic->setValue($char);
-                    $characteristic->setCTypeId($tid);
-                    $characteristic->setGood($good);
-
-                    $em = $this->getDoctrine()->getManager();
-                    $em->persist($characteristic);
-                    $em->flush();
-
-                }
-            }
-
-        }
-        //return new Response('Характеристики для '.$good->getName().' успешно спарсены');
-    }
 }
---
+++ src/Nurix/CatalogBundle/Parser/Parser.php
@@ -0,0 +1,174 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: root
+ * Date: 4/28/13
+ * Time: 11:14 PM
+ * To change this template use File | Settings | File Templates.
+ */
+namespace Nurix\CatalogBundle\Parser;
+use DateTime;
+use Nurix\CatalogBundle\Entity\Goods;
+use Doctrine\ORM\EntityNotFoundException;
+use Nurix\CatalogBundle\Entity\Characteristic;
+use Nurix\CatalogBundle\Entity\CharacteristicSection;
+use Nurix\CatalogBundle\Entity\CharacteristicType;
+use Symfony\Component\HttpFoundation\File\File;
+use Sunra\PhpSimple\HtmlDomParser;
+
+class Parser
+{
+    private $xlsx_service;
+
+    private $doctrine_service;
+
+    public function __construct($doctrine_service, $xlsx_service)
+    {
+        $this->xlsx_service = $xlsx_service;
+        $this->doctrine_service = $doctrine_service;
+    }
+
+    public function parseExcel(File $file)
+    {
+
+        $exelObj = $this->xlsx_service->load($file);
+        $sheetData = $exelObj->getActiveSheet()->toArray(null, true, true, true);
+        $index = 0;
+
+        $entityManager = $this->doctrine_service->getManager();
+
+        $catalogs = $entityManager->getRepository('CatalogBundle:Catalog')->findAll();
+        $goods_alias = array();
+
+        $added_goods = 0;
+        $updated_goods = 0;
+
+        foreach ($catalogs as $catalog) {
+            $goods_alias[$catalog->getId()] = explode(',', $catalog->getGoodsAlias());
+        }
+
+        foreach ($sheetData as $sheetRow) {
+
+            $index++;
+            if ($index == 1) continue;
+            $article = $sheetRow['A'] ? $sheetRow['A'] : null;
+            $name = $sheetRow['B'] ? $sheetRow['B'] : null;
+            $last_update = new DateTime(date('Y-m-d', strtotime($sheetRow['I'])));
+            $price = (float)$sheetRow['L'];
+            $urlYandex = $sheetRow['Q'];
+            $subcatalog = null;
+            if (!empty($article)) {
+
+                foreach ($goods_alias as $catalog_id => $aliases) {
+                    foreach ($aliases as $alias) {
+                        if (!empty($alias))
+                            if (strpos($name, $alias) !== false) {
+                                $subcatalog = $entityManager->getRepository('CatalogBundle:Catalog')->find($catalog_id);
+                                break;
+                            }
+                    }
+                }
+
+                $good = $entityManager->getRepository('CatalogBundle:Goods')->findOneByArticle($article);
+                if ($good) {
+                    $updated_goods++;
+                    $good->setName($name);
+                    $good->setLastUpdate($last_update);
+                    $good->setPrice($price);
+                    $good->setCatalog($subcatalog);
+                    $good->setActive(true);
+                    $entityManager->flush();
+
+
+                } else {
+                    $added_goods++;
+                    $good = new Goods();
+                    $good->setArticle($article);
+                    $good->setName($name);
+                    $good->setActive(true);
+                    $good->setLastUpdate($last_update);
+                    $good->setPrice($price);
+                    $good->setCatalog($subcatalog);
+
+                    $entityManager->persist($good);
+                    $entityManager->flush();
+
+
+                }
+
+                if ($urlYandex) {
+                    $characters = $entityManager->getRepository('CatalogBundle:Characteristic')->findByGoodId($good->getId());
+                    if (!$characters) {
+                        $this->parseYandex($urlYandex, $good->getId());
+                    }
+                }
+            }
+        }
+        return array('added' => $added_goods, 'updated' => $updated_goods);
+    }
+
+    public function parseYandex($url, $goodid)
+    {
+        $good = $this->doctrine_service->getRepository('CatalogBundle:Goods')->find($goodid);
+        if (!$good) {
+            Throw new EntityNotFoundException("good not found");
+        }
+        $html = HtmlDomParser::file_get_html($url);
+        foreach ($html->find('table.l-page_layout_72-20 .b-properties') as $table) {
+            $sid = 0;
+            foreach ($table->find('tr') as $tr) {
+                $section_type = null;
+                $html_span = HtmlDomParser::str_get_html($tr);
+                $section_type = $html_span->find('.b-properties__title', -1);
+
+                if ($section_type != null) {
+                    $section_type = $section_type->plaintext;
+                    $repository = $this->doctrine_service->getRepository('CatalogBundle:CharacteristicSection');
+                    $characteristic_section = $repository->findOneBySectionvalue($section_type);
+                    if ($characteristic_section) {
+                        $sid = $characteristic_section;
+                    } else {
+                        $section = new CharacteristicSection();
+                        $section->setSectionvalue($section_type);
+
+                        $em = $this->doctrine_service->getManager();
+                        $em->persist($section);
+                        $em->flush();
+                        $sid = $section;
+                    }
+                } else {
+                    $char_type = $html_span->find('.b-properties__label-title', -1)->plaintext;
+
+                    $char = $html_span->find('.b-properties__value', -1)->plaintext;
+                    $repository = $this->doctrine_service->getRepository('CatalogBundle:CharacteristicType');
+                    $type = $repository->findOneByName($char_type);
+
+                    if ($type) {
+                        $tid = $type;
+
+                    } else {
+                        $characteristic_type = new CharacteristicType();
+                        $characteristic_type->setName($char_type);
+                        $characteristic_type->setSection($sid);
+
+                        $em = $this->doctrine_service->getManager();
+                        $em->persist($characteristic_type);
+                        $em->flush();
+                        $tid = $characteristic_type;
+                    }
+                    $characteristic = new Characteristic();
+                    $characteristic->setValue($char);
+                    $characteristic->setCTypeId($tid);
+                    $characteristic->setGood($good);
+
+                    $em = $this->doctrine_service->getManager();
+                    $em->persist($characteristic);
+                    $em->flush();
+
+                }
+            }
+
+        }
+        //return new Response('Характеристики для '.$good->getName().' успешно спарсены');
+    }
+}
