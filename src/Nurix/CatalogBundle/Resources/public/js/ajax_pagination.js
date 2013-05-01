f56b4ea41465a414277204721a335f4be1e6273a
Nurlanbek Moldomurov <bupychuk1989@gmail.com>
2013-05-01 22:20:27 +0600

    NUR-15 jquery.click ne veshaets9 na elementy zagrujennye cherez ajax

--- src/Nurix/CatalogBundle/Resources/public/js/ajax_pagination.js
+++ src/Nurix/CatalogBundle/Resources/public/js/ajax_pagination.js
@@ -1,5 +1,5 @@
 $(function () {
-    $('#showMoreRnd').click(function () {
+    $(document).on('click','#showMoreRnd',function () {
         var path = $(this).attr("href");
         $.ajax({
             url: path,
@@ -11,7 +11,7 @@ $(function () {
                 var $new = $(data);
                 $('#all-catalog .navigation').remove();
                 $('#all-catalog').append($new);
-                $new.show('slow');
+                $new.show(1000);
             }
         });
         return false;
