9c103095f8da9b58601bc07dda94dbfc52c132b9
Aza <azamatkurmanbekov@gmail.com>
2013-05-18 18:18:23 +0600

    NUR-34 problema so ssylkami reshena

--- src/Nurix/NurixBundle/Controller/DefaultController.php
+++ src/Nurix/NurixBundle/Controller/DefaultController.php
@@ -23,8 +23,53 @@ class DefaultController extends Controller
                     $filename = sha1(uniqid(mt_rand(), true));
                     $filename = $filename . '.' . $extension;
                     $file->move($dir, $filename);
-                    return new Response($dir.'/'.$filename);
+
+                    $result = $this->SendCKEditorResults(0,$request->query->get('CKEditorFuncNum'),"/nurix/web/upload/ckimages/".$filename,$filename);
+
+                    return new Response($result);
                 }
         }
+    }
+    function SendCKEditorResults ($errorNumber, $CKECallback, $fileUrl, $fileName, $customMsg ='')
+    {
+        $result ="";
+        // Minified version of the document.domain automatic fix script (#1919).
+        // The original script can be found at _dev/domain_fix_template.js
+        $result .= <<<EOF
+<script type="text/javascript">
+(function(){var d=document.domain;while (true){try{var A=window.parent.document.domain;break;}catch(e) {};d=d.replace(/.*?(?:\.|$)/,'');if (d.length==0) break;try{document.domain=d;}catch (e){break;}}})();
+EOF;
+
+        if ($errorNumber && $errorNumber != 201) {
+            $fileUrl = "";
+            $fileName= "";
+        }
+
+        $msg = "";
+
+        switch ($errorNumber )
+        {
+            case 0 :
+                $msg = "Upload successful";
+                break;
+            case 1 :	// Custom error.
+                $msg = $customMsg;
+                break ;
+            case 201 :
+                $msg = 'A file with the same name is already available. The uploaded file has been renamed to "' . $fileName . '"' ;
+                break ;
+            case 202 :
+                $msg = 'Invalid file' ;
+                break ;
+            default :
+                $msg = 'Error on file upload. Error number: ' + $errorNumber ;
+                break ;
+        }
+
+        $rpl = array( '\\' => '\\\\', '"' => '\\"' ) ;
+        $result .= 'window.parent.CKEDITOR.tools.callFunction("'. $CKECallback. '","'. strtr($fileUrl, $rpl). '", "'. strtr( $msg, $rpl). '");' ;
+
+        $result .= '</script>';
+        return $result;
     }
 }
