aef7da4f217836cb6158295aad10cc6c9aec6f7f
BupycHuk <bupychuk1989@gmail.com>
2013-09-03 17:27:58 +0600

    ck_sonataMediaTest

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -53,6 +53,7 @@ class AppKernel extends Kernel
             new Nurix\PageBundle\NurixPageBundle(),
             new Nurix\SliderBundle\NurixSliderBundle(),
             new FOS\JsRoutingBundle\FOSJsRoutingBundle(),
+            new CoopTilleuls\Bundle\CKEditorSonataMediaBundle\CoopTilleulsCKEditorSonataMediaBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
