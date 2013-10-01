fd4f4d4d687b84f0de9ed742c8a7e9fe16c498f8
Nurlan Moldomurov <bupychuk1989@gmail.com>
2013-10-01 12:23:53 +0600

    slugify

--- src/Application/Sonata/NewsBundle/Entity/Tag.php
+++ src/Application/Sonata/NewsBundle/Entity/Tag.php
@@ -41,27 +41,39 @@ class Tag extends BaseTag
 
     public static function slugify($text)
     {
-        // replace non letter or digits by -
-        $text = preg_replace('~[^\\pL\d]+~u', '-', $text);
+        $text = mb_strtolower($text, 'utf-8');
 
-        // trim
-        $text = trim($text, '-');
+        // replace cyrillic chars
+        $text = Tag::replaceCyrillic($text);
 
-        // transliterate
-        if (function_exists('iconv')) {
-            $text = iconv('utf-8', 'us-ascii//TRANSLIT', $text);
-        }
+        // strip all non word chars
+        $text = preg_replace('/\W/', ' ', $text);
 
-        // lowercase
-        $text = strtolower($text);
+        // replace all white space sections with a dash
+        $text = preg_replace('/\ +/', '-', $text);
 
-        // remove unwanted characters
-        $text = preg_replace('~[^-\w]+~', '', $text);
-
-        if (empty($text)) {
-            return str_random(10);
-        }
+        // trim dashes
+        $text = preg_replace('/\-$/', '', $text);
+        $text = preg_replace('/^\-/', '', $text);
 
         return $text;
+    }
+
+    public static function replaceCyrillic ($text)
+    {
+        $chars = array(
+            'ґ'=>'g','ё'=>'e','є'=>'e','ї'=>'i','і'=>'i',
+            'а'=>'a', 'б'=>'b', 'в'=>'v',
+            'г'=>'g', 'д'=>'d', 'е'=>'e', 'ё'=>'e',
+            'ж'=>'zh', 'з'=>'z', 'и'=>'i', 'й'=>'i',
+            'к'=>'k', 'л'=>'l', 'м'=>'m', 'н'=>'n',
+            'о'=>'o', 'п'=>'p', 'р'=>'r', 'с'=>'s',
+            'т'=>'t', 'у'=>'u', 'ф'=>'f', 'х'=>'h',
+            'ц'=>'c', 'ч'=>'ch', 'ш'=>'sh', 'щ'=>'sch',
+            'ы'=>'y', 'э'=>'e', 'ю'=>'u', 'я'=>'ya', 'é'=>'e', '&'=>'and',
+            'ь'=>'', 'ъ' => '',
+        );
+
+        return strtr($text, $chars);
     }
 }
