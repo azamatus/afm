d34cc7023a0ce408ce12b114f3ea55d9497c5bb9
Mederbek <juve-meder@mail.ru>
2014-02-20 22:37:32 +0600

    NUR-2 vrode kak gotovo

--- src/Nurix/CatalogBundle/Controller/AdminController.php
+++ src/Nurix/CatalogBundle/Controller/AdminController.php
@@ -63,5 +63,49 @@ class AdminController extends CRUDController
             ));
     }
 
+    public function excelGoodsUploadAction(Request $request)
+    {
+        $form = $this->createForm(new ExcelType());
+
+        if ($request->isMethod('POST')) {
+            $form->bind($request);
+
+            if ($form->isValid()) {
+                $file = $form['file']->getData();
+                $extension = null;
+                $original_file = $file->getClientOriginalName();
+
+                if (stripos($original_file, 'xls')) {
+                    $dir = $this->get('kernel')->getRootDir() . '/../web/price';
+
+                    $filename = sha1(uniqid(mt_rand(), true));
+                    $filename = $filename . '.' . $extension;
+                    $file->move($dir, $filename);
+                    $excel_file = new File($dir . '/' . $filename);
+
+                    $info = $this->get('catalog.product.goods_upload_parser')->parseExcel($excel_file);
+
+                    $added_message = 'Добавлено: ' . $info['added'];
+                    $updated_message = 'Обновлено: ' . $info['updated'];
+                    $this->get('session')->getFlashBag()->add('notice', $added_message);
+                    $this->get('session')->getFlashBag()->add('notice', $updated_message);
+                } else {
+                    $this->get('session')->getFlashBag()->add('notice', 'Неверный формат');
+                }
+
+                $this->redirect($this->container->get('sonata.admin.goods')->generateUrl('excel'));
+
+            }
+        }
+
+        return $this->render('CatalogBundle:Admin:excel.html.twig',
+            array('form' => $form->createView(),
+                'base_template' => $this->getBaseTemplate(),
+                'admin' => $this->container->get('sonata.admin.goods'),
+                'admin_pool'      => $this->container->get('sonata.admin.pool'),
+                'blocks'          => $this->container->getParameter('sonata.admin.configuration.dashboard_blocks')
+            ));
+    }
+
 
 }
