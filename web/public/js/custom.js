13b9991a65b50f6878ca4f08a80be64efb264d67
Aza <azamatkurmanbekov@gmail.com>
2014-04-10 15:17:19 +0600

    NRZ-16 мультиязычность в js

--- web/public/js/custom.js
+++ web/public/js/custom.js
@@ -204,53 +204,7 @@ function changeQuantityCatalog(){
         $.cookie("cookieQuantity", $(this).val(), { expires: 3, path: '/' });
         location.reload();
     });
-
 }
-
-function validateRegistrationForm(){
-    $('#form_registration form').validate({
-        rules: {
-            "fos_user_registration_form[username]":{
-                required: true
-            },
-            "fos_user_registration_form[email]":{
-                required: true,
-                email: true
-            },
-            "fos_user_registration_form[firstname]":{
-                required: true
-            },
-            "fos_user_registration_form[lastname]":{
-                required: true
-            },
-            "fos_user_registration_form[plainPassword][first]":{
-                required: true
-            },
-            "fos_user_registration_form[plainPassword][second]":{
-                required: true,
-                equalTo: "#fos_user_registration_form_plainPassword_first"
-            }
-        },
-        errorPlacement: function(error, element) {
-            if (element.hasClass("error")){
-                element.next().remove();
-                element.parent().parent().addClass('error');
-                element.parent().append("<span class='help-inline'><i class='icon-remove'></i>  Неверные данные</span>");
-                if(element.attr('id') == 'fos_user_registration_form_plainPassword_second'){
-                    element.parent().parent().addClass('error');
-                    element.next().remove();
-                    element.parent().append('<span class="help-inline"><i class="icon-remove"></i>  Пароли не совпадают!</span>');
-                }
-            }else {
-                element.parent().parent().removeClass('error');
-                element.next().remove();
-            }
-        },
-        success: function() {
-        }
-    })
-}
-
 $(document).ready(function () {
     changeCatalogStyle();
     changeCurrency();
@@ -267,5 +221,4 @@ $(document).ready(function () {
     rangePriceSlider();
     sortable();
     changeQuantityCatalog();
-    validateRegistrationForm();
 });
